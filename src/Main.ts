18f33c174036f95a52eac08e6078d764766f9761
ashayx <973946691@qq.com>
2018-11-17 17:14:52 +0800

    test

--- src/Main.ts
+++ src/Main.ts
@@ -103,6 +103,7 @@ class Main extends eui.UILayer {
      * Create scene interface
      */
     protected createGameScene(): void {
+        log(111)
         App.GameController.startGame(App.LayerManager.GAME_SCENE_LAYER, App.LayerManager.GAME_UI_LAYER);
     }
 }
